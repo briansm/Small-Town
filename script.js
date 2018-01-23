8fd371b0db9ce873f65dfaf8b38053900ae21539
Brian Morales <briansmor09@gmail.com>
2018-01-22 19:14:16 -0600

    Agregando fondo

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var vp=document.getElementById("villa");
+var papel=vp.getContext("2d");
+var mapa="tile.png";
+
+var imagen=new Image();
+fondo.src=mapa;
+fondo.addEventListener("load",dibujar);
+
+function dibujar(){
+  papel.drawImage(fondo,0,0);
+}
 
 function aleatorio(min,max){
   var resultado=Math.floor(Math.random()*(max-min+1))+min;
