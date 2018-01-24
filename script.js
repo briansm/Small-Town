ab97aa84535067b8e61c98c70cf6210c8effa157
Brian Morales <briansmor09@gmail.com>
2018-01-23 20:34:52 -0600

    Carga ok de vaca

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 var vp=document.getElementById("villa");
 var papel=vp.getContext("2d");
-var mapa="tile.png";
 
-var fondo=new Image();
-fondo.src=mapa;
-fondo.addEventListener("load",dibujar);
 
-var cerdo=new Image();
-cerdo.src="cerdo.png";
-cerdo.addEventListener("load",dibcerdo);
-
-var pollo=new Image();
-pollo.src="pollo.png";
-pollo.addEventListener("load",dibpollo);
-
-var vaca=new Image();
-vaca.src="vaca.png";
-vaca.addEventListener("load",dibvaca);
-
-function dibujar(){
-  papel.drawImage(fondo,0,0);
+var fondo={
+  url: "tile.png",
+  cargaOK: false
 }
-
-function dibcerdo(){
-  papel.drawImage(cerdo,0,0);
+var vaca ={
+  url: "vaca.png",
+  cargaOK: false
 }
+fondo.imagen=new Image();
+fondo.imagen.src=fondo.url;
+fondo.imagen.addEventListener("load",dibfondo);
 
-function dibpollo(){
-  papel.drawImage(pollo,0,0);
+vaca.imagen=new Image();
+vaca.imagen.src=vaca.url;
+vaca.imagen.addEventListener("load",dibvaca);
+
+function dibfondo(){
+  fondo.cargaOK=true;
+  dibujar();
 }
 
 function dibvaca(){
-  papel.drawImage(vaca,0,0);
+  vaca.cargaOK=true;
+  dibujar();
 }
+
+function dibujar(){
+  if(fondo.cargaOK==true){
+    papel.drawImage(fondo.imagen,0,0);
+  }
+  if(vaca.cargaOK==true){
+    papel.drawImage(vaca.imagen,100,100);
+  }
+}
+
+
 
 function aleatorio(min,max){
   var resultado=Math.floor(Math.random()*(max-min+1))+min;
